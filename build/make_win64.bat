63df56b2aabae225851a01d16850f90023a1b29c
Kevin <xyu.kevin@gmail.com>
2021-03-04 19:27:26 +0800

    update bat for windows

--- build/make_win64.bat
+++ build/make_win64.bat
@@ -1,5 +1,5 @@
 mkdir build_win_64tool & pushd build_win_64tool
-cmake ../src -G "Visual Studio 16 2019" -T v141 -A x64 -DNEKOFS_MAKE_TOOLS_LIB=ON
+cmake .. -G "Visual Studio 16 2019" -T v141 -A x64 -DNEKOFS_MAKE_TOOLS_LIB=ON
 popd
 cmake --build build_win_64tool --config Release
 pause
